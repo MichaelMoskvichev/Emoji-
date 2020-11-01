a2632752a219c722e55cf9843668761276851185
Mikhail Moskvichev <mikhailmos22@yandex.ru>
2020-11-01 10:44:37 +0300

    create start screen

---
+++ Emoji Reader/EmojiTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiTableViewController.swift
+//  Emoji Reader
+//
+//  Created by Миша Москвичев on 01.11.2020.
+//
+
+import UIKit
+
+class EmojiTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+}
