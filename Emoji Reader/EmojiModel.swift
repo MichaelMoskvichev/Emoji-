7a5b86c5d1c82757c17791653ee423ec72d1e8ac
Mikhail Moskvichev <mikhailmos22@yandex.ru>
2020-11-02 20:03:31 +0300

    basic satings

---
+++ Emoji Reader/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  Emoji Reader
+//
+//  Created by Миша Москвичев on 02.11.2020.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji : String
+    var name : String
+    var description : String
+    var isFavourite : Bool
+}
---
+++ Emoji Reader/EmojiTabaleViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmojiTabaleViewCell.swift
+//  Emoji Reader
+//
+//  Created by Миша Москвичев on 01.11.2020.
+//
+
+import UIKit
+
+class EmojiTabaleViewCell: UITableViewCell {
+
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
