2654dad7e76a78e0dc42871f1c208deab0aaae47
Mikhail Moskvichev <mikhailmos22@yandex.ru>
2020-11-07 14:06:52 +0300

    SDGADG

--- Emoji Reader/NewEmojiTableViewController.swift
+++ Emoji Reader/NewEmojiTableViewController.swift
@@ -9,20 +9,31 @@ import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
 
-    @IBOutlet weak var emojiTextField: UIView!
+    @IBOutlet weak var emojiTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       updateSabeButtonState()
+    }
+    
+    // реализация функцианала кнопки SAVE (горит тогда когда все 3 графы заполнены)
+    private func updateSabeButtonState() {
+        let emojiText = emojiTextField.text ?? ""
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descriptionTextField.text ?? ""
         
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
     }
+    
+    
     @IBAction func textChanged(_ sender: UITextField) {
+       updateSabeButtonState()
     }
+    
     
   
 }
